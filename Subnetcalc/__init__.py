b5954db152232bd8c61de1b24af8720e89e99b8a
nsutherland78 <nathan.sutherland@gmail.com>
2016-01-20 18:53:16 -0500

    Formatted subnetwork outputs, added logic to ensure a non-specified prefix using -p doesn't show /None in the output.  Modified shortcut to specify prefix to list in subnetwork output.

--- Subnetcalc/__init__.py
+++ Subnetcalc/__init__.py
@@ -13,10 +13,10 @@ def create_parser():
         help='Enter an IP network in NN.NN.NN.NN/SS, NN.NN.NN.NN/SS.SS.SS.SS or NN.NN.NN.NN/WW.WW.WW.WW notation'
     )
     parser.add_argument(
-        '-n',
-        '--newprefix',
+        '-p',
+        '--prefix',
         type=int,
-        help='This value specifies the size of the subnetworks to display in the subnetworks'
+        help='This value specifies the provided networks prefix length to display in the subnetworks output'
     )
     return parser.parse_args()
 
@@ -27,16 +27,20 @@ def main():
     ipnetwork = ARGS.network
     hostrange = list(ipaddress.IPv4Network(ipnetwork).hosts())
     supernet = ipaddress.IPv4Network(ipnetwork).supernet()
-    newprefix = ARGS.newprefix
+    newprefix = ARGS.prefix
     subnetworks = list(ipaddress.IPv4Network(ipnetwork).subnets(new_prefix=newprefix))
+    if newprefix is None:
+        newprefix = ipaddress.IPv4Network(ipnetwork).prefixlen
     print('Network or Host Provided:        {}'.format(ipnetwork))
-    print('Network and Broadcast Addresses: {}, {}'.format(ipaddress.IPv4Network(ipnetwork).network_address, ipaddress.IPv4Network(ipnetwork).broadcast_address))
     print('CIDR Notation:                   {}'.format(ipaddress.IPv4Network(ipnetwork).with_prefixlen))
     print('Standard Mask:                   {}'.format(ipaddress.IPv4Network(ipnetwork).with_netmask))
     print('Wildcard Mask:                   {}'.format(ipaddress.IPv4Network(ipnetwork).with_hostmask))
-    print('First to Last Host in Subnet:    {} - {}'.format(hostrange[0], hostrange[-1]))
-    print('Supernet of network is:          {}'.format(supernet))
-    print('Subnetworks are:                 {}'.format(subnetworks))
+    print('Network and Broadcast IPs:       {}, {}'.format(ipaddress.IPv4Network(ipnetwork).network_address, ipaddress.IPv4Network(ipnetwork).broadcast_address))
+    print('Usable IPs in Network:           {} - {}'.format(hostrange[0], hostrange[-1]))
+    print('Supernet of Network Is:          {}'.format(supernet))
+    print('/{} Subnetworks Are:'.format(newprefix))
+    for networks in subnetworks:
+        print(networks)
 
 if __name__ == '__main__':
     main()
