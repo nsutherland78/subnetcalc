ed15601c3e010850049862384128254f434ee782
Nathan Sutherland <nsutherland@blackberry.com>
2016-01-28 17:11:13 -0500

    Updated setup.py file

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(name="subnetcalc",
       description="subnet calculator tool",
-      author="author email",
-      auther_email="authoremail",
+      author="Nathan Sutherland",
+      author_email="authoremail",
       packages=find_packages(),
       entry_points={
         'console_scripts':  [
